504425de390deaf972974aef9acbeafaeadc6b73
Marco Cardoso <marcom.c@outlook.com>
2020-11-23 19:09:58 +0000

    Update main module

--- cron_execution_dates/main.py
+++ cron_execution_dates/main.py
@@ -1,9 +1,9 @@
-from cron_execution_dates.utils import argument_reader
+from utils import argument_reader, cron_reader
 
 if __name__ == "__main__":
     time_arg = argument_reader.get_current_time_arg()
     current_datetime = argument_reader.get_current_datetime(time_arg)
 
-    cron_cmd = argument_reader.read_cron_cmd()
-    while cron_cmd is not None:
-        cron_cmd = argument_reader.read_cron_cmd()
+    cron = cron_reader.read_cron_cmd()
+    while cron is not None:
+        cron = cron_reader.read_cron_cmd()
