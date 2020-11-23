2c1b12d192ed18bd8559e700761d7b8f922a9872
Marco Cardoso <marcom.c@outlook.com>
2020-11-23 23:45:28 +0000

    Document time.py

--- cron_execution_dates/utils/time.py
+++ cron_execution_dates/utils/time.py
@@ -1,6 +1,16 @@
-def valid_minute(minute: int):
+def valid_minute(minute: int) -> bool:
+    """
+    Validate if the given minute is within the range 0-59
+    :param minute: An integer representing the minute
+    :return: A boolean representing the validation
+    """
     return (minute >= 0) & (minute <= 59)
 
 
-def valid_hour(hour: int):
+def valid_hour(hour: int) -> bool:
+    """
+    Validate if the given hour is within the range 0-23
+    :param hour: An integer representing the hour
+    :return: A boolean representing the validation
+    """
     return (hour >= 0) & (hour <= 23)
