e87befd25082f2128dac2e2c95aa0cf136eb98e0
Marco Cardoso <marcom.c@outlook.com>
2020-11-23 17:57:31 +0000

    Method to read the CRON commands

--- cron_execution_dates/main.py
+++ cron_execution_dates/main.py
@@ -1,6 +1,9 @@
-def main():
-    pass
-
+from cron_execution_dates.utils import argument_reader
 
 if __name__ == "__main__":
-    main()
+    time_arg = argument_reader.get_current_time_arg()
+    current_datetime = argument_reader.get_current_datetime(time_arg)
+
+    cron_cmd = argument_reader.read_cron_cmd()
+    while cron_cmd is not None:
+        cron_cmd = argument_reader.read_cron_cmd()
